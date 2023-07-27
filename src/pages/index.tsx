f92fd8b77df7b093d1bdc5606781ba338b18cdb8
valyy151 <marin.valenta1@gmail.com>
2023-07-27 18:20:10 +0200

    homepage bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import { useSession } from "next-auth/react";
 import Heading from "~/components/ui/Heading";
-import Spinner from "~/components/ui/Spinner";
 
 export default function Home() {
   const { data, status } = useSession();
-
-  if (!data) {
-    return <Spinner />;
-  }
 
   return (
     <main className="mt-8 text-center text-2xl">
